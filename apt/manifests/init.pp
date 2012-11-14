a7098e46cc7b0c7ec5bf04b5b8c93be4b191e003
Jaromir Muller <jaromir.muller@gmail.com>
2012-11-14 14:02:53 +0200

    update apt commands

--- apt/manifests/init.pp
+++ apt/manifests/init.pp
@@ -1,7 +1,12 @@
 class apt {
 
-    exec { "/usr/bin/aptitude -y safe-upgrade":
-         refreshonly => true,
-    }
-
+	exec { "apt_update":
+		command => "/usr/bin/apt-get update",
+	}
+	
+	exec { "aptitude_safe_upgrade":
+		command => "/usr/bin/aptitude -y safe-upgrade",
+		refreshonly => true,
+	}
+	
 }
